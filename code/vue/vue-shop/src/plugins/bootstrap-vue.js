6815a85ccbe74e9b9c837c8dc2ce8ed168774f02
Chisthoper Mella <chisthoper96@gmail.com>
2021-10-20 15:40:50 -0300

    app shop vue

---
+++ code/vue/vue-shop/src/plugins/bootstrap-vue.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import BooststrapVue, { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
